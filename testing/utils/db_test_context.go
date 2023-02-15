a3cc6aa44e264878302208db5e632b6fa896012b
Luis Silva <luis.silva@chiselstrike.com>
2023-02-15 17:53:58 -0300

    test: add end-to-end tests executing commands on database (#2)

---
+++ testing/utils/db_test_context.go
@@ -0,0 +1,55 @@
+package utils
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	qt "github.com/frankban/quicktest"
+	"github.com/spf13/cobra"
+
+	"github.com/chiselstrike/libsql-shell/src/cmd"
+)
+
+type DbTestContext struct {
+	*testing.T
+	*qt.C
+
+	dbPath  string
+	rootCmd *cobra.Command
+}
+
+func NewTestContext(t *testing.T) *DbTestContext {
+	return &DbTestContext{t, qt.New(t), t.TempDir() + "test.sqlite", cmd.NewRootCmd()}
+}
+
+func (tc *DbTestContext) Execute(statements string) (string, error) {
+	return Execute(tc.T, tc.rootCmd, "--db", tc.dbPath, "--exec", statements)
+}
+
+func (tc *DbTestContext) CreateEmptySimpleTable(tableName string) {
+	_, err := tc.Execute("CREATE TABLE " + tableName + " (id INTEGER PRIMARY KEY, textField TEXT, intField INTEGER)")
+	tc.Assert(err, qt.IsNil)
+}
+
+type SimpleTableEntry struct {
+	TextField string
+	IntField  int
+}
+
+func (tc *DbTestContext) CreateSimpleTable(tableName string, initialValues []SimpleTableEntry) {
+	tc.CreateEmptySimpleTable(tableName)
+
+	if len(initialValues) == 0 {
+		return
+	}
+
+	values := make([]string, 0, len(initialValues))
+	for _, initialValue := range initialValues {
+		values = append(values, fmt.Sprintf("('%v', %v)", initialValue.TextField, initialValue.IntField))
+	}
+	insertQuery := "INSERT INTO " + tableName + "(textField, intField) VALUES " + strings.Join(values, ",")
+
+	_, err := tc.Execute(insertQuery)
+	tc.Assert(err, qt.IsNil)
+}
