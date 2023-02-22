e76ee1d121a01ea3c605c8e64c3b25493b6e7adf
Ricardo Dalarme <ricardo.dalarme@profusion.mobi>
2023-02-22 13:56:17 -0300

    test(shell): add tests to the interactive shell (#7)

---
+++ testing/root_command_shell_test.go
@@ -0,0 +1,54 @@
+package main_test
+
+import (
+	"testing"
+
+	"github.com/chiselstrike/libsql-shell/src/lib"
+	"github.com/chiselstrike/libsql-shell/testing/utils"
+	qt "github.com/frankban/quicktest"
+)
+
+func TestRootCommandShell_WhenNoCommandsAreProvided_ExpectShellExecutedWithoutError(t *testing.T) {
+	tc := utils.NewTestContext(t)
+
+	result, err := tc.ExecuteShell([]string{})
+
+	tc.Assert(err, qt.IsNil)
+	tc.Assert(result, qt.Equals, "")
+}
+
+func TestRootCommandShell_WhenExecuteInvalidStatement_ExpectError(t *testing.T) {
+	tc := utils.NewTestContext(t)
+
+	result, err := tc.ExecuteShell([]string{"SELECTT 1;"})
+
+	tc.Assert(err, qt.IsNil)
+	tc.Assert(result, qt.Equals, "Error: near \"SELECTT\": syntax error")
+}
+
+func TestRootCommandShell_WhenCreateTable_ExpectDbHaveTheTable(t *testing.T) {
+	tc := utils.NewTestContext(t)
+
+	result, err := tc.ExecuteShell([]string{"CREATE TABLE test (name STRING);", "SELECT * FROM test;"})
+
+	tc.Assert(err, qt.IsNil)
+	tc.Assert(result, qt.Equals, "name")
+}
+
+func TestRootCommandShell_WhenCreateTableAndInsertData_ExpectDbHaveTheTableWithTheData(t *testing.T) {
+	tc := utils.NewTestContext(t)
+
+	result, err := tc.ExecuteShell([]string{"CREATE TABLE test (name STRING);", "INSERT INTO test VALUES ('test');", "SELECT * FROM test;"})
+
+	tc.Assert(err, qt.IsNil)
+	tc.Assert(result, qt.Equals, "name\ntest")
+}
+
+func TestRootCommandShell_WhenTypingQuitCommand_ExpectShellNotRunFollowingCommands(t *testing.T) {
+	tc := utils.NewTestContext(t)
+
+	result, err := tc.ExecuteShell([]string{lib.QUIT_COMMAND, "SELECT 1;"})
+
+	tc.Assert(err, qt.IsNil)
+	tc.Assert(result, qt.Equals, "")
+}
--- testing/utils/utils.go
+++ testing/utils/utils.go
@@ -9,13 +9,25 @@ import (
 )
 
 func Execute(t *testing.T, c *cobra.Command, args ...string) (string, error) {
+	return ExecuteWithInitialInput(t, c, "", args...)
+}
+
+func ExecuteWithInitialInput(t *testing.T, c *cobra.Command, initialInput string, args ...string) (string, error) {
 	t.Helper()
 
 	buf := new(bytes.Buffer)
+	bufIn := new(bytes.Buffer)
+
+	_, err := bufIn.Write([]byte(initialInput))
+	if err != nil {
+		return "", err
+	}
+
 	c.SetOut(buf)
 	c.SetErr(buf)
+	c.SetIn(bufIn)
 	c.SetArgs(args)
 
-	err := c.Execute()
+	err = c.Execute()
 	return strings.TrimSpace(buf.String()), err
 }
