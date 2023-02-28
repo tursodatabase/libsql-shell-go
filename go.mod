0aef52712027d2db4beeae82ee9293b90c600b25
Luis Silva <luis.silva@chiselstrike.com>
2023-02-28 15:28:01 -0300

    change(testing): implement test suites to shell and exec test (#14)
    Also adds a teardown command to DbTestContext to drop all tables after each test

--- go.mod
+++ go.mod
@@ -7,11 +7,15 @@ require (
 	github.com/libsql/sqld/packages/golang/libsql-client v0.0.0-20230301144623-3c86897b436d
 	github.com/mattn/go-sqlite3 v1.14.16
 	github.com/spf13/cobra v1.6.1
+	github.com/stretchr/testify v1.8.2
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/sys v0.0.0-20220310020820-b874c991c1a5 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 require (
--- testing/utils/db_test_context.go
+++ testing/utils/db_test_context.go
@@ -6,7 +6,6 @@ import (
 	"testing"
 
 	qt "github.com/frankban/quicktest"
-	"github.com/spf13/cobra"
 
 	"github.com/chiselstrike/libsql-shell/src/cmd"
 )
@@ -15,20 +14,21 @@ type DbTestContext struct {
 	*testing.T
 	*qt.C
 
-	dbPath  string
-	rootCmd *cobra.Command
+	dbPath string
 }
 
-func NewTestContext(t *testing.T) *DbTestContext {
-	return &DbTestContext{t, qt.New(t), t.TempDir() + "test.sqlite", cmd.NewRootCmd()}
+func NewTestContext(t *testing.T, dbPath string) *DbTestContext {
+	return &DbTestContext{T: t, C: qt.New(t), dbPath: dbPath}
 }
 
 func (tc *DbTestContext) Execute(statements string) (string, error) {
-	return Execute(tc.T, tc.rootCmd, "--exec", statements, tc.dbPath)
+	rootCmd := cmd.NewRootCmd()
+	return Execute(tc.T, rootCmd, "--exec", statements, tc.dbPath)
 }
 
 func (tc *DbTestContext) ExecuteShell(commands []string) (string, error) {
-	return ExecuteWithInitialInput(tc.T, tc.rootCmd, strings.Join(commands, "\n"), tc.dbPath)
+	rootCmd := cmd.NewRootCmd()
+	return ExecuteWithInitialInput(tc.T, rootCmd, strings.Join(commands, "\n"), tc.dbPath, "--quiet")
 }
 
 func (tc *DbTestContext) CreateEmptySimpleTable(tableName string) {
@@ -56,4 +56,28 @@ func (tc *DbTestContext) CreateSimpleTable(tableName string, initialValues []Sim
 
 	_, err := tc.Execute(insertQuery)
 	tc.Assert(err, qt.IsNil)
+}
+
+func (tc *DbTestContext) DropTable(tableName string) {
+	_, err := tc.Execute("DROP TABLE " + tableName)
+	tc.Assert(err, qt.IsNil)
+}
+
+func (tc *DbTestContext) getAllTables() []string {
+	result, err := tc.ExecuteShell([]string{".tables"})
+	tc.Assert(err, qt.IsNil)
+	if strings.TrimSpace(result) == "" {
+		return []string{}
+	}
+	return strings.Split(result, "\n")
+}
+
+func (tc *DbTestContext) dropAllTables() {
+	for _, createdTable := range tc.getAllTables() {
+		tc.DropTable(createdTable)
+	}
+}
+
+func (tc *DbTestContext) TearDown() {
+	tc.dropAllTables()
 }
