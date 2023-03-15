5bef08bcc8288e043a21c2513502a37119cb2bd6
ricardodalarme <ricardo.dalarme@profusion.mobi>
2023-03-15 14:36:14 -0300

    refactor(utils): move IsHttpUrl to the utils folder (#43)

---
+++ src/lib/utils.go
@@ -0,0 +1,11 @@
+package lib
+
+import "net/url"
+
+func IsHttpUrl(path string) bool {
+	url, err := url.ParseRequestURI(path)
+	if err != nil {
+		return false
+	}
+	return url.Scheme == "http" || url.Scheme == "https"
+}
