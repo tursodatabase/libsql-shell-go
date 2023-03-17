010b2e5004f70f7dccaa3785ce9d8097f72032b1
Stefano Saffran <stefano@chiselstrike.com>
2023-03-17 10:57:06 -0400

    feat: add indexes command

--- shell/databaseRootCommand.go
+++ shell/databaseRootCommand.go
@@ -36,7 +36,7 @@ func NewDatabaseRootCmd(config *dbCmdConfig) *cobra.Command {
 		},
 	}
 
-	rootCmd.AddCommand(tableCmd, schemaCmd, helpCmd, readCmd)
+	rootCmd.AddCommand(tableCmd, schemaCmd, helpCmd, readCmd, indexesCmd)
 	rootCmd.SetOut(config.OutF)
 	rootCmd.SetErr(config.ErrF)
 	rootCmd.SetHelpTemplate(helpTemplate)
