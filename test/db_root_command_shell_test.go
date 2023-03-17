f584ee3ddb5249e66e67bdf4ddcb4966be99d357
Stefano Saffran <stefano@chiselstrike.com>
2023-03-17 11:05:22 -0400

    feat: add tests for indexes command

--- test/db_root_command_shell_test.go
+++ test/db_root_command_shell_test.go
@@ -84,7 +84,7 @@ func (s *DBRootCommandShellSuite) Test_WhenCallDotHelpCommand_ExpectAListWithAll
 	outS, errS, err := s.tc.ExecuteShell([]string{".help"})
 	s.tc.Assert(err, qt.IsNil)
 	s.tc.Assert(errS, qt.Equals, "")
-	s.tc.Assert(outS, qt.Equals, ".help       List of all available commands.\n  .read       Execute commands from a file\n  .schema     Show table schemas.\n  .tables     List all existing tables in the database.\n  .quit       Exit this program.")
+	s.tc.Assert(outS, qt.Equals, ".help       List of all available commands.\n  .indexes    List indexes in a table or database\n  .read       Execute commands from a file\n  .schema     Show table schemas.\n  .tables     List all existing tables in the database.\n  .quit       Exit this program.")
 }
 
 func (s *DBRootCommandShellSuite) Test_GivenAEmptyDb_WhenCallDotReadCommand_ExpectToSeeATableWithOneEntry() {
@@ -108,6 +108,63 @@ func (s *DBRootCommandShellSuite) Test_GivenAEmptyDb_WhenCallDotReadCommandPassi
 	s.tc.Assert(err, qt.IsNil)
 	s.tc.Assert(errS, qt.Equals, "Error: open nonExistingFile.txt: no such file or directory")
 	s.tc.Assert(outS, qt.Equals, "")
+}
+
+func (s *DBRootCommandShellSuite) Test_GivenADBWithTwoTables_WhenCreateTwoIndexesAndCallDotIndexesCommand_ExpectToReturnTheIndexes() {
+	s.tc.CreateSimpleTable("simple_table", []utils.SimpleTableEntry{{TextField: "value1", IntField: 1}, {TextField: "value2", IntField: 2}})
+	s.tc.CreateSimpleTable("simple_table_2", []utils.SimpleTableEntry{{TextField: "value1", IntField: 1}, {TextField: "value2", IntField: 2}})
+
+	_, errS, err := s.tc.Execute("CREATE INDEX idx_textfield on simple_table (TextField);CREATE INDEX idx_intfield on simple_table_2 (IntField)")
+	s.tc.Assert(err, qt.IsNil)
+	s.tc.Assert(errS, qt.Equals, "")
+
+	outSchema, errS, err := s.tc.ExecuteShell([]string{".indexes"})
+	s.tc.Assert(err, qt.IsNil)
+	s.tc.Assert(errS, qt.Equals, "")
+	s.tc.Assert(outSchema, qt.Equals, utils.GetPrintTableOutput([]string{""}, [][]string{{"idx_textfield\nidx_intfield"}}))
+}
+
+func (s *DBRootCommandShellSuite) Test_GivenADBWithThreeTables_WhenCreateThreeIndexesAndCallDotIndexesCommandPassingExactTableName_ExpectToReturnJustOneIndex() {
+	s.tc.CreateSimpleTable("simple_table", []utils.SimpleTableEntry{{TextField: "value1", IntField: 1}, {TextField: "value2", IntField: 2}})
+	s.tc.CreateSimpleTable("simple_table_2", []utils.SimpleTableEntry{{TextField: "value1", IntField: 1}, {TextField: "value2", IntField: 2}})
+	s.tc.CreateSimpleTable("simple_table_3", []utils.SimpleTableEntry{{TextField: "value1", IntField: 1}, {TextField: "value2", IntField: 2}})
+
+	_, errS, err := s.tc.Execute("CREATE INDEX idx_textfield on simple_table (TextField);CREATE INDEX idx_intfield on simple_table_2 (IntField);CREATE INDEX idx_intfield_third_table on simple_table_3 (IntField)")
+	s.tc.Assert(err, qt.IsNil)
+	s.tc.Assert(errS, qt.Equals, "")
+
+	outSchema, errS, err := s.tc.ExecuteShell([]string{".indexes simple_table_3"})
+	s.tc.Assert(err, qt.IsNil)
+	s.tc.Assert(errS, qt.Equals, "")
+	s.tc.Assert(outSchema, qt.Equals, utils.GetPrintTableOutput([]string{""}, [][]string{{"idx_intfield_third_table"}}))
+}
+
+func (s *DBRootCommandShellSuite) Test_GivenADBWithThreeTables_WhenCreateThreeIndexesAndCallDotIndexesCommandPassingPartOfTableName_ExpectToReturnTwoIndexes() {
+	s.tc.CreateSimpleTable("simple_table", []utils.SimpleTableEntry{{TextField: "value1", IntField: 1}, {TextField: "value2", IntField: 2}})
+	s.tc.CreateSimpleTable("simple_table_2", []utils.SimpleTableEntry{{TextField: "value1", IntField: 1}, {TextField: "value2", IntField: 2}})
+	s.tc.CreateSimpleTable("another_simple_table", []utils.SimpleTableEntry{{TextField: "value1", IntField: 1}, {TextField: "value2", IntField: 2}})
+
+	_, errS, err := s.tc.Execute("CREATE INDEX idx_textfield on simple_table (TextField);CREATE INDEX idx_intfield on simple_table_2 (IntField);CREATE INDEX idx_intfield_another_table on another_simple_table (IntField)")
+	s.tc.Assert(err, qt.IsNil)
+	s.tc.Assert(errS, qt.Equals, "")
+
+	outSchema, errS, err := s.tc.ExecuteShell([]string{".indexes simple%"})
+	s.tc.Assert(err, qt.IsNil)
+	s.tc.Assert(errS, qt.Equals, "")
+	s.tc.Assert(outSchema, qt.Equals, utils.GetPrintTableOutput([]string{""}, [][]string{{"idx_textfield\nidx_intfield"}}))
+}
+
+func (s *DBRootCommandShellSuite) Test_GivenADBWithATable_WhenCreateAIndexAndCallDotIndexesCommandPassingAWrongTableName_ExpectEmptyReturn() {
+	s.tc.CreateSimpleTable("simple_table", []utils.SimpleTableEntry{{TextField: "value1", IntField: 1}, {TextField: "value2", IntField: 2}})
+
+	_, errS, err := s.tc.Execute("CREATE INDEX idx_textfield on simple_table (TextField);")
+	s.tc.Assert(err, qt.IsNil)
+	s.tc.Assert(errS, qt.Equals, "")
+
+	outSchema, errS, err := s.tc.ExecuteShell([]string{".indexes nonExistingTable"})
+	s.tc.Assert(err, qt.IsNil)
+	s.tc.Assert(errS, qt.Equals, "")
+	s.tc.Assert(outSchema, qt.Equals, utils.GetPrintTableOutput([]string{""}, [][]string{{""}}))
 }
 
 func (s *DBRootCommandShellSuite) Test_WhenCallACommandThatDoesNotExist_ExpectToReturnAnErrorMessage() {
