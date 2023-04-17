90338134da348fa5c2fdbb6d517047389685f801
Stefano Saffran <stefano@chiselstrike.com>
2023-04-17 15:50:59 -0400

    feat(shell-command): add mode command

---
+++ shell/commands/mode.go
@@ -0,0 +1,30 @@
+package commands
+
+import (
+	"fmt"
+
+	"github.com/libsql/libsql-shell-go/shell/enums"
+	"github.com/spf13/cobra"
+)
+
+var modeCmd = &cobra.Command{
+	Use:   ".mode MODE",
+	Short: "Set output mode",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		config, ok := cmd.Context().Value(dbCtx{}).(*DbCmdConfig)
+		if !ok {
+			return fmt.Errorf("missing db connection")
+		}
+		mode := args[0]
+		switch mode {
+		case string(enums.TABLE_MODE):
+			config.SetMode(enums.TABLE_MODE)
+		case string(enums.CSV_MODE):
+			config.SetMode(enums.CSV_MODE)
+		default:
+			return fmt.Errorf("unsupported mode")
+		}
+		return nil
+	},
+}
