d999351762323634a2422e0571dceae409dc2284
Luis Silva <luis.silva@chiselstrike.com>
2023-04-28 14:29:49 -0300

    refactor(db): iterate over result sets in case query returns multiple results

--- internal/db/db.go
+++ internal/db/db.go
@@ -63,7 +63,7 @@ func (db *Db) Close() {
 
 func (db *Db) ExecuteStatements(statementsString string) (StatementsResult, error) {
 
-	statements, err := sqlparser.SplitStatementToPieces(statementsString)
+	queries, err := sqlparser.SplitStatementToPieces(statementsString)
 	if err != nil {
 		return StatementsResult{}, err
 	}
@@ -72,34 +72,15 @@ func (db *Db) ExecuteStatements(statementsString string) (StatementsResult, erro
 
 	go func() {
 		defer close(statementResultCh)
-		db.executeStatementsAndPopulateChannel(statements, statementResultCh)
+		db.executeQueriesAndPopulateChannel(queries, statementResultCh)
 	}()
 
 	return StatementsResult{StatementResultCh: statementResultCh}, nil
 }
 
-func (db *Db) executeStatementsAndPopulateChannel(statements []string, statementResultCh chan StatementResult) {
-	rowsEndedWithoutErrorCh := make(chan bool)
-	defer close(rowsEndedWithoutErrorCh)
-
-	for _, statement := range statements {
-		result := db.executeStatement(statement, rowsEndedWithoutErrorCh)
-
-		if result == nil {
-			continue
-		}
-
-		if result.Err != nil {
-			if strings.Contains(result.Err.Error(), "interactive transaction not allowed in HTTP queries") {
-				result.Err = &shellerrors.TransactionNotSupportedError{}
-			}
-			statementResultCh <- *result
-			return
-		}
-
-		statementResultCh <- *result
-		shouldContinue := <-rowsEndedWithoutErrorCh
-		if !shouldContinue {
+func (db *Db) executeQueriesAndPopulateChannel(queries []string, statementResultCh chan StatementResult) {
+	for _, query := range queries {
+		if shouldContinue := db.executeQuery(query, statementResultCh); !shouldContinue {
 			return
 		}
 	}
@@ -119,67 +100,23 @@ func (db *Db) ExecuteAndPrintStatements(statementsString string, outF io.Writer,
 	return nil
 }
 
-func (db *Db) executeStatement(statement string, rowsEndedWithoutErrorCh chan bool) *StatementResult {
-	if strings.TrimSpace(statement) == "" {
-		return nil
+func (db *Db) executeQuery(query string, statementResultCh chan StatementResult) (queryEndedWithoutError bool) {
+	if strings.TrimSpace(query) == "" {
+		return true
 	}
 
-	rows, err := db.sqlDb.Query(statement)
+	rows, err := db.sqlDb.Query(query)
 	if err != nil {
-		return &StatementResult{Err: err}
-	}
-
-	columnNames, err := getColumnNames(rows)
-	if err != nil {
-		return &StatementResult{Err: err}
-	}
-
-	columnTypes, err := getColumnTypes(rows)
-	if err != nil {
-		return &StatementResult{Err: err}
-	}
-
-	columnNamesLen := len(columnNames)
-	columnPointers := make([]interface{}, columnNamesLen)
-	for i, t := range columnTypes {
-		if t.Kind() == reflect.Struct {
-			columnPointers[i] = reflect.New(t).Interface()
-		} else {
-			columnPointers[i] = new(interface{})
-		}
-	}
-
-	rowCh := make(chan rowResult)
-	go func() {
-		defer rows.Close()
-		defer close(rowCh)
-
-		for rows.Next() {
-			err = rows.Scan(columnPointers...)
-			if err != nil {
-				rowCh <- rowResult{Err: err}
-				rowsEndedWithoutErrorCh <- false
-				return
-			}
-
-			rowData := make([]interface{}, len(columnTypes))
-			for i, ptr := range columnPointers {
-				val := reflect.ValueOf(ptr).Elem()
-				rowData[i] = val.Interface()
-			}
-			rowCh <- rowResult{Row: rowData}
+		if strings.Contains(err.Error(), "interactive transaction not allowed in HTTP queries") {
+			err = &shellerrors.TransactionNotSupportedError{}
 		}
 
-		if err := rows.Err(); err != nil {
-			rowCh <- rowResult{Err: err}
-			rowsEndedWithoutErrorCh <- false
-			return
-		}
+		statementResultCh <- StatementResult{Err: err}
+		return false
+	}
+	defer rows.Close()
 
-		rowsEndedWithoutErrorCh <- true
-	}()
-
-	return &StatementResult{ColumnNames: columnNames, RowCh: rowCh}
+	return readQueryResults(rows, statementResultCh)
 }
 
 func getColumnNames(rows *sql.Rows) ([]string, error) {
@@ -208,4 +145,86 @@ func getColumnTypes(rows *sql.Rows) ([]reflect.Type, error) {
 	}
 
 	return types, nil
+}
+
+func readQueryResults(queryRows *sql.Rows, statementResultCh chan StatementResult) (queryEndedWithoutError bool) {
+	rowsEndedWithoutErrorCh := make(chan bool)
+	defer close(rowsEndedWithoutErrorCh)
+
+	hasResultSetToRead := true
+	for hasResultSetToRead {
+		statementResult := readQueryResultSet(queryRows, rowsEndedWithoutErrorCh)
+		statementResultCh <- statementResult
+
+		if statementResult.Err != nil {
+			return false
+		}
+
+		shouldContinue := <-rowsEndedWithoutErrorCh
+		if !shouldContinue {
+			return false
+		}
+
+		hasResultSetToRead = queryRows.NextResultSet()
+	}
+
+	if err := queryRows.Err(); err != nil {
+		statementResultCh <- StatementResult{Err: err}
+		return false
+	}
+
+	return true
+}
+
+func readQueryResultSet(queryRows *sql.Rows, rowsEndedWithoutErrorCh chan bool) StatementResult {
+	columnNames, err := getColumnNames(queryRows)
+	if err != nil {
+		return StatementResult{Err: err}
+	}
+
+	columnTypes, err := getColumnTypes(queryRows)
+	if err != nil {
+		return StatementResult{Err: err}
+	}
+
+	columnNamesLen := len(columnNames)
+	columnPointers := make([]interface{}, columnNamesLen)
+	for i, t := range columnTypes {
+		if t.Kind() == reflect.Struct {
+			columnPointers[i] = reflect.New(t).Interface()
+		} else {
+			columnPointers[i] = new(interface{})
+		}
+	}
+
+	rowCh := make(chan rowResult)
+	go func() {
+		defer close(rowCh)
+
+		for queryRows.Next() {
+			err = queryRows.Scan(columnPointers...)
+			if err != nil {
+				rowCh <- rowResult{Err: err}
+				rowsEndedWithoutErrorCh <- false
+				return
+			}
+
+			rowData := make([]interface{}, len(columnTypes))
+			for i, ptr := range columnPointers {
+				val := reflect.ValueOf(ptr).Elem()
+				rowData[i] = val.Interface()
+			}
+			rowCh <- rowResult{Row: rowData}
+		}
+
+		if err := queryRows.Err(); err != nil {
+			rowCh <- rowResult{Err: err}
+			rowsEndedWithoutErrorCh <- false
+			return
+		}
+
+		rowsEndedWithoutErrorCh <- true
+	}()
+
+	return StatementResult{ColumnNames: columnNames, RowCh: rowCh}
 }
