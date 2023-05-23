d768e5ac64bcbfeef44d81129b073e6ee8311aae
Luis Silva <luis.silva@chiselstrike.com>
2023-05-23 09:04:54 -0300

    feat(suggester): don't suggest keywords when everything is accepted

---
+++ internal/suggester/tokenRulesFinder.go
@@ -0,0 +1,37 @@
+package suggester
+
+import (
+	"github.com/antlr/antlr4/runtime/Go/antlr/v4"
+	"github.com/libsql/sqlite-antlr4-parser/sqliteparser"
+)
+
+type TokenRulesFinder struct {
+	sqliteparser.BaseSQLiteParserListener
+
+	targetToken antlr.Token
+
+	tokenRules []int
+}
+
+func NewTokenRulesFinder(targetToken antlr.Token) *TokenRulesFinder {
+	return &TokenRulesFinder{targetToken: targetToken, tokenRules: make([]int, 0)}
+}
+
+func (trf *TokenRulesFinder) EnterEveryRule(ctx antlr.ParserRuleContext) {
+	ruleStartTokenIndex := ctx.GetStart().GetTokenIndex()
+	ruleEndTokenIndex := ctx.GetStop().GetTokenIndex()
+	targetTokenIndex := trf.targetToken.GetTokenIndex()
+
+	if ruleStartTokenIndex <= targetTokenIndex && targetTokenIndex <= ruleEndTokenIndex {
+		trf.tokenRules = append(trf.tokenRules, ctx.GetRuleIndex())
+	}
+}
+
+func getTokenRules(tree sqliteparser.IParseContext, targetToken antlr.Token) []int {
+
+	tokenRulesFinder := NewTokenRulesFinder(targetToken)
+
+	antlr.ParseTreeWalkerDefault.Walk(tokenRulesFinder, tree)
+
+	return tokenRulesFinder.tokenRules
+}
