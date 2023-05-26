f63a075aca2f50a24e7ed54fb295f9de95c5b725
Douglas Montes <douglas.montes@profusion.mobi>
2023-05-26 17:09:54 -0300

    feat(mode-cmd): provide more help for .mode command
    - if user provides no argument for MODE or an invalid one, prints an error message that
      informs the current mode and displays the valid modes
    - if the user provides a valid mode, silently updates the current MODE
      to the provided one

--- internal/shellcmd/mode.go
+++ internal/shellcmd/mode.go
@@ -2,6 +2,7 @@ package shellcmd
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/libsql/libsql-shell-go/pkg/shell/enums"
 	"github.com/spf13/cobra"
@@ -10,11 +11,21 @@ import (
 var modeCmd = &cobra.Command{
 	Use:   ".mode MODE",
 	Short: "Set output mode",
-	Args:  cobra.ExactArgs(1),
+	Args:  cobra.MaximumNArgs(1),
+	ValidArgs: []string{
+		string(enums.TABLE_MODE),
+		string(enums.JSON_MODE),
+		string(enums.CSV_MODE),
+	},
 	RunE: func(cmd *cobra.Command, args []string) error {
+		validModes := strings.Join(cmd.ValidArgs, ", ")
 		config, ok := cmd.Context().Value(dbCtx{}).(*DbCmdConfig)
 		if !ok {
 			return fmt.Errorf("missing db connection")
+		}
+		currentMode := config.GetMode()
+		if len(args) == 0 {
+			return fmt.Errorf("No mode provided. Current mode is %s. Valid modes are %s", currentMode, validModes)
 		}
 		mode := args[0]
 		switch mode {
@@ -25,7 +36,7 @@ var modeCmd = &cobra.Command{
 		case string(enums.JSON_MODE):
 			config.SetMode(enums.JSON_MODE)
 		default:
-			return fmt.Errorf("unsupported mode")
+			return fmt.Errorf("Invalid mode. Current mode is %s. Valid modes are %s", currentMode, validModes)
 		}
 		return nil
 	},
